30cb02ffa44a00992a8282b83a8ce5130f53c4f3
Vitaliy Shevtsov <vit_shevtsov@mail.ru>
2022-03-14 18:57:24 +0300

    feat: bug with noMatch route

--- src/routes/NoMatchRoute.tsx
+++ src/routes/NoMatchRoute.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NoMatchRoute() {
   return (
     <main style={{ padding: '1rem' }}>
-      <p>There's nothing here!</p>
+      <p>There is nothing here!</p>
     </main>
   );
 }
