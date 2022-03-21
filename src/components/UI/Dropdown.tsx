783f49df1f5948acaea8434aca98abd5fed32ac9
Vitaliy Shevtsov <vit_shevtsov@mail.ru>
2022-03-21 12:34:12 +0300

    add: toggle dropdown items

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -1,50 +1,35 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react';
+import React, { useState } from 'react';
 import NameInput from './NameInput';
 import styles from './Dropdown.module.css';
 import { categories } from '../../routes/Categories';
 
-const Dropdown: React.FC = () => (
-  <div className={styles.dropdownWrapper}>
-    <NameInput
-      placeholder="Введите категорию"
-      label="Категория"
-    />
-    {/* {categories.map(item => item)}</div> */}
-    <div className="dropdownToggle" />
-    <div className="dropdownItems" />
-  </div>
+const dropdownArrowIcon = require('../../assets/iconDropdownArrow.png');
+
+const dropdownItems = (
+  <ul className={styles.dropdownItems}>
+    {categories.map((item) => <li key={item.id}>{item.name}</li>)}
+  </ul>
 );
+const Dropdown: React.FC = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const toggleItems = () => {
+    setIsOpened(!isOpened);
+  };
+
+  return (
+    <div className={styles.dropdownWrapper}>
+      <NameInput
+        placeholder="Выберите категорию"
+        label="Категория"
+        readonly
+        onClickHandler={toggleItems}
+      />
+      <img className={styles.arrowIcon} src={dropdownArrowIcon} alt="dropdown arrow icon" />
+      <div className="dropdownToggle" />
+      {isOpened && dropdownItems}
+    </div>
+  );
+};
 
 export default Dropdown;
-
-{ /* <div class="dropdown-wrapper" v-click-outside="close">
-    <div class="dropdown-toggle" @click="toggle">
-      <slot name="dropdown-toggle" />
-    </div>
-    <div class="dropdown-items" v-show="isOpened" @click="close">
-      <slot name="dropdown-items" />
-    </div>
-  </div> */ }
-
-{ /* <dropdown>
-            <template #dropdown-toggle>
-              <my-input
-                id="passport-foreign_country"
-                label="Страна выдачи"
-                :value="formData.passportForeign.country"
-                @input="debouncedCountriesOfIssue"
-              />
-            </template>
-
-            <template #dropdown-items>
-              <div
-                v-for="item in filteredCountriesOfIssue"
-                :key="item + countriesList.indexOf(item)"
-                class="dropdown-item"
-                @click="selectedCountry(item)"
-              >
-                {{ item }}
-              </div>
-            </template>
-          </dropdown> */ }
--- src/components/UI/NameInput.tsx
+++ src/components/UI/NameInput.tsx
@@ -7,7 +7,15 @@ export default function NameInput(props: any) {
 // todo знак обязательности красным
   return (
     <div className={styles.inputWrapper}>
-      <input type="text" id="name" placeholder={props.placeholder} maxLength={255} className={styles.input} />
+      <input
+        type="text"
+        id="name"
+        placeholder={props.placeholder}
+        maxLength={255}
+        className={styles.input}
+        readOnly={props.readonly}
+        onClick={props.onClickHandler}
+      />
       { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
       <label htmlFor="name" className={styles.label}>{props.isRequired ? `${props.label}*` : props.label}</label>
     </div>
