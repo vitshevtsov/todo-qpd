4cf5b33ca8d8ada23bf7173e19081ceee70f9864
Vitaliy Shevtsov <vit_shevtsov@mail.ru>
2022-03-24 18:11:20 +0300

    feat: fix styles and min screenWidth / screenHeight

--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-// import { IModalContext, ModalContext } from './context/modalContext';
-// import ModalContext from './context/modalContext';
 
-// async function getData() {
-//   try {
-//     const response = await fetch('https://api.punkapi.com/v2/beers', {mode: 'no-cors'});
-//     const data = await response.json();
-//     return data;
-//   } catch(error) {
-//     alert(`Error: ${error}`)
-//   }
-// };
-
-// const data = getData();
-// console.log(data);
-// const initialModalContext: IModalContext = {
-//   isAddTaskOpen: false,
-//   isAddCategoryOpen: false,
-// };
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const isScreenSupported = (screenWidth >= 1440) && (screenHeight >= 900);
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <ModalContext.Provider value={initialModalContext}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </ModalContext.Provider> */}
+    {isScreenSupported
+      ? (
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      )
+      : <div>Извините, разрешение Вашего экрана не поддерживается</div>}
+
   </React.StrictMode>,
   document.getElementById('root'),
 );
